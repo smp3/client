7c5f1f5ae2ab2dfe94307909d2c8755182e75530
maciek <maciej.zakrzewski87@gmail.com>
2015-10-29 15:59:24 +0100

    fetching playlists

--- js/controllers/playlist.js
+++ js/controllers/playlist.js
@@ -1,6 +1,14 @@
 Smp3Controllers.controller('Smp3PlaylistCtrl', ['$scope', '$location', '$http', 'store', 'PlayerService', 'PlaylistService',
     function ($scope, $location, $http, store, player, playlist) {
         playlist.bindScope($scope);
+
+        $scope.getPlaylists = function () {
+            console.log('playlists');
+            $http.get('/api/playlists').success(function (data) {
+                $scope.playlists = data;
+            });
+        };
+
         $scope.play = function (index) {
             player.stop();
             playlist.setPointer(index);
@@ -10,5 +18,12 @@ Smp3Controllers.controller('Smp3PlaylistCtrl', ['$scope', '$location', '$http', 
         $scope.delete = function (index) {
             playlist.delete(index);
         };
+
+        $scope.setCurrentPlaylist = function(playlist) {
+            $scope.queue = playlist;
+            console.log('Current playlist', playlist);
+        };
+
+        $scope.getPlaylists();
 
     }]);
