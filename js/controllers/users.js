eaadf13bdccc5ee8eeb1a5445a363e23d899a856
maciek <maciej.zakrzewski87@gmail.com>
2015-11-03 15:36:31 +0100

    user delete

--- js/controllers/users.js
+++ js/controllers/users.js
@@ -5,42 +5,55 @@ Smp3Controllers.controller('Smp3UsersCtrl', ['$scope', '$location', '$http', 'st
                 $scope.users = data;
             });
         };
-        
-        $scope.getUser = function() {
+
+        $scope.getUser = function () {
             console.log('user_id', $routeParams.id);
-            if(!$routeParams.id) {
+            if (!$routeParams.id) {
                 $scope.user = {
-                    
                 };
             }
-            $http.get('/api/users/'+$routeParams.id).success(function (data) {
-               $scope.user = data;
-               console.log($scope.user);
+            $http.get('/api/users/' + $routeParams.id).success(function (data) {
+                $scope.user = data;
+                console.log($scope.user);
             });
         };
-        
+
+        $scope.deleteUser = function (user) {
+            $http({
+                url: '/api/users/' + user.id,
+                method: 'DELETE'
+            }).then(function (response) {
+                console.log(response);
+                $scope.getAllUsers();
+            },
+                    function (response) {
+                        console.log(response);
+                    }
+            );
+        };
+
         $scope.saveUser = function (user) {
-           var url, method;
-           if(user.id) {
-               url = '/api/users/'+user.id;
-               method = 'PUT';
-           } else {
-               url = '/api/users';
-               method = 'POST';
-           }
+            var url, method;
+            if (user.id) {
+                url = '/api/users/' + user.id;
+                method = 'PUT';
+            } else {
+                url = '/api/users';
+                method = 'POST';
+            }
             $http({
                 url: url,
                 method: method,
                 data: user
             }).then(function (response) {
                 console.log('Repsonse', response);
-                
+
 
             }, function (response) {
                 console.log('Repsonse', response);
 
             });
         };
-        
+
 
     }]);
