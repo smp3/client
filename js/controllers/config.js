0b37c1c21c050989087078140f20fc4190469b97
maciek <maciej.zakrzewski87@gmail.com>
2015-11-03 16:27:52 +0100

    some styling fixes

--- js/controllers/config.js
+++ js/controllers/config.js
@@ -1,5 +1,6 @@
 Smp3Controllers.controller('Smp3ConfigCtrl', ['$scope', '$location', '$http', 'store',
     function ($scope, $location, $http, store) {
+        $scope.config = store.get('config');
         $scope.saveConfig = function (config) {
             store.set('config', config);
             $location.path('/');
