7877366539d0c11af2e4b81526db27b14d554cbe
maciek <maciej.zakrzewski87@gmail.com>
2015-11-09 10:15:54 +0100

    main layout changes

--- js/controllers/info.js
+++ js/controllers/info.js
@@ -1,5 +1,10 @@
 Smp3Controllers.controller('Smp3InfoCtrl', ['$scope', 'DiscogsService',
     function ($scope, discogs) {
+        $scope.visible = true;
+        $scope.toggle = function () {
+            $scope.visible = !$scope.visible;
+        };
+        
         discogs.initialize();
         discogs.bindScope($scope);
     }]);
